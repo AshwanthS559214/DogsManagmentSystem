c75520191e135459a6ca04bd584e794e63263e41
Ashwanth_s559214 <123043659+AshwanthS559214@users.noreply.github.com>
2023-11-25 22:50:35 -0600

    Add files via upload

---
+++ src/main/java/com/example/DMS/repository/TrainerRepository.java
@@ -0,0 +1,13 @@
+package com.example.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.DMS.Models.Trainer;
+/*
+ * @author Ashwanth Reddy Cheemarla
+ */
+
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
